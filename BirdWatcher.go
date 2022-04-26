211a80ae379aecfb5612dbc48d208c51c1c554ec
Brahim Berkasse <berkassebrahim@gmail.com>
2022-04-26 01:29:46 +0000

    🦤 For Loops

---
+++ BirdWatcher.go
@@ -0,0 +1,41 @@
+package birdwatcher
+
+// TotalBirdCount return the total bird count by summing
+// the individual day's counts.
+func TotalBirdCount(birdsPerDay []int) int {
+	var total int;
+    
+    for i := 0; i < len(birdsPerDay); i++ {
+        total += birdsPerDay[i];
+    }
+	return (total);
+}
+
+// BirdsInWeek returns the total bird count by summing
+// only the items belonging to the given week.
+func BirdsInWeek(birdsPerDay []int, week int) int {
+	var birdsPerWeek int;
+    var trackWeek = 0;
+
+    for i := 0; i < len(birdsPerDay); i++ {
+        if(i % 7 == 0){
+            trackWeek++;
+        }
+    	if(trackWeek == week){
+            birdsPerWeek += birdsPerDay[i];
+        }
+    }
+	return (birdsPerWeek);
+}
+
+// FixBirdCountLog returns the bird counts after correcting
+// the bird counts for alternate days.
+func FixBirdCountLog(birdsPerDay []int) []int {
+    for i := 0; i < len(birdsPerDay); i++ {
+        if(i % 2 == 0){
+            birdsPerDay[i] = birdsPerDay[i] + 1;
+        }
+    }
+	return (birdsPerDay);
+}
+
